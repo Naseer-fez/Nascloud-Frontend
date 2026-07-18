bd7e1d756ac0ab7d4f39076e86d30976078ec45b
Naseer <feznaseer777@gmail.com>
2026-07-18 22:53:32 +0530

    chore: disable console logging in frontend

--- src/utils/testLogger.js
+++ src/utils/testLogger.js
@@ -1,9 +1,5 @@
 const PREFIX = '#fez';
 
 export function logFez(message, details) {
-  if (details === undefined) {
-    console.log(PREFIX, message);
-    return;
-  }
-  console.log(PREFIX, message, details);
+  // Console logging disabled
 }
